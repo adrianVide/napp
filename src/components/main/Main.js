bfe1658f8e0a096297aec2741b0a87c66678956f
Adrian Vide <adrian.vide.paris@gmail.com>
2020-07-24 15:56:24 +0200

    fetching and showing data

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Main = () => {
-    return (
-        <div>
-            testing main
-        </div>
-    )
-}
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await axios('https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas')
+      setData(response.data.results)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      testing main
+      <ul>
+        {data.map((worker) => (
+          <li key={worker.id}>
+            <a href={worker.id}>{worker.first_name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
-// import App from './App';
 import * as serviceWorker from "./serviceWorker";
 import { Main } from "./components/main/Main";
 import { Worker } from "./components/worker/Worker";
@@ -17,7 +16,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
