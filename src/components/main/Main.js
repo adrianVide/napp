7a502be9b4421077a8b2a9a4571928b95d084482
Adrian Vide <adrian.vide.paris@gmail.com>
2020-07-24 16:06:50 +0200

    first name filtering working

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,22 +4,44 @@ import axios from "axios";
 
 export const Main = () => {
   const [data, setData] = useState([]);
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     async function fetchData() {
-      let response = await axios('https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas')
-      setData(response.data.results)
+      let response = await axios(
+        "https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas"
+      );
+      setData(response.data.results);
     }
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
+
+  const filteredData = data.filter((worker) =>
+    worker.first_name.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  )
+
+  console.log(filteredData)
 
   return (
     <div>
       testing main
+      <form className="">
+        <input
+          type="text"
+          placeholder="Search for..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          name="query"
+        />
+      </form>
       <ul>
         {data.map((worker) => (
           <li key={worker.id}>
-            <a href={worker.id}>{worker.first_name}</a>
+            <a href={worker.id}>
+              {worker.first_name}
+              {worker.last_name}
+            </a>
+            <li>{worker.profession}</li>
           </li>
         ))}
       </ul>
